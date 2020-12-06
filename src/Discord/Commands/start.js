eaf85a38a809aa4780b62c4858a2b59293d685f7
arrendek <arrendek@gmail.com>
2020-12-05 19:29:27 -0500

    Remove DBL and time limit on server for auto-disbanding game

--- src/Discord/Commands/start.js
+++ src/Discord/Commands/start.js
@@ -5,19 +5,19 @@ module.exports = {
     description: "Start the game!",
     permissions: Util.addBits(Util.permissions.requiresGame, Util.permissions.requiresGameMaster),
     exe(message, args, handler, game) {
-       if (game.started) return;
-       if (game.teamHasOneMember()) return message.channel.send("**✖ | At least 2 players in every team are required!**");
-       if (!game.teamsHaveSpymasters()) return message.channel.send("**✖ | All teams must have a spymaster assigned!**");
-       game.start();
-       const interval = setInterval(() => {
-             const stillGame = handler.games.get(message.channel.id);
-             if (stillGame && stillGame.id === game.id) {
-               if ((Date.now() - stillGame.lastAction) > 1200000 || stillGame.players.size === 0) {
-                   stillGame.stop();
-                   clearInterval(interval);
-                   return message.channel.send("** 📤 | Game disbanded. **");
-               }
-             }else clearInterval(interval);
-       }, 240000);
+        if (game.started) return;
+        if (game.teamHasOneMember()) return message.channel.send("**✖ | At least 2 players in every team are required!**");
+        if (!game.teamsHaveSpymasters()) return message.channel.send("**✖ | All teams must have a spymaster assigned!**");
+        game.start();
+        /*const interval = setInterval(() => {
+            const stillGame = handler.games.get(message.channel.id);
+            if (stillGame && stillGame.id === game.id) {
+                if ((Date.now() - stillGame.lastAction) > 1200000 || stillGame.players.size === 0) {
+                    stillGame.stop();
+                    clearInterval(interval);
+                    return message.channel.send("** 📤 | Game disbanded. **");
+                }
+            } else clearInterval(interval);
+        }, 240000);*/
     }
 }
--- src/Discord/Events/ready.js
+++ src/Discord/Events/ready.js
@@ -1,8 +1,8 @@
 
-const {DBL} = require("../../Util/Util.js");
+//const {DBL} = require("../../Util/Util.js");
 
 module.exports = (handler) => {
-    DBL(process.env.DBL, handler);
+    //DBL(process.env.DBL, handler);
     handler.user.setActivity("Prefix: -", {type: "PLAYING"});
     console.log(`READY!\n\nCommands: ${handler.commands.size}\nGuilds: ${handler.guilds.cache.size}\nUsers: ${handler.users.cache.size}`);
 }
